857e7f68739751b3a93351a39809cbc34e76e294
Anjali Patel <88501227+anjalipatel101@users.noreply.github.com>
2025-03-11 17:35:44 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./screens/Home";
+import Menu from "./screens/Menu";
+import Cart from "./screens/Cart";
+import OrderStatus from "./screens/OrderStatus";
+import Login from "./screens/Login";
+import { AuthProvider } from "./context/AuthContext";
+import "./styles/App.css";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
-function App() {
+const App: React.FC = () => {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -18,23 +26,42 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo. THIS IS A TEST TO SEE IF IT WILL AUTO UPDATE!!
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <AuthProvider>
+      <Router>
+        <div className="app-container">
+          <header className="app-header">
+            <h1>🍕 Pizza Ordering App</h1>
+          </header>
+          <nav className="app-nav">
+            <a href="/">Home</a>
+            <a href="/menu">Menu</a>
+            <a href="/cart">Cart</a>
+            <a href="/order-status">Order Status</a>
+            <a href="/login">Login</a>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/order-status" element={<OrderStatus />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <main className="todo-section">
+            <h2>🍕 Customize Your Pizza</h2>
+            <button onClick={createTodo}>+ Add Custom Pizza</button>
+            <ul>
+              {todos.map((todo) => (
+                <li key={todo.id}>{todo.content}</li>
+              ))}
+            </ul>
+          </main>
+          <footer className="app-footer">
+            <p>© 2025 Pizza Ordering App</p>
+          </footer>
+        </div>
+      </Router>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
